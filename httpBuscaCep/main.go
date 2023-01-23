e56276a9fddfdc25085f612d31092ff5591924b1
J Claudio Tomás Jr <jclaudiotomas@gmail.com>
2023-01-22 21:24:38 -0300

    Via cep servidor http

--- httpBuscaCep/main.go
+++ httpBuscaCep/main.go
@@ -1,13 +1,77 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type ViaCep struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+//URL: http://viacep.com.br/ws/01001000/json/
 
 func main() {
 
-	http.HandleFunc("/", BuscaCep)
+	http.HandleFunc("/", BuscaCepHandler)
 	http.ListenAndServe(":3000", nil)
 }
 
-func BuscaCep(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	cep, error := BuscaCep(cepParam)
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//w.Write([]byte("Hello World!"))
+
+	// result, err := json.Marshal(cep)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	return
+	// }
+	// w.Write(result)
+	json.NewEncoder(w).Encode(cep)
+
+}
+
+func BuscaCep(cep string) (*ViaCep, error) {
+	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := ioutil.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+	var c ViaCep
+	error = json.Unmarshal(body, &c)
+	if error != nil {
+		return nil, error
+	}
+	return &c, nil
+
 }
